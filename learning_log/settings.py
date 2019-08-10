c50aa3cddbb20279c5e57475cbe9a25501a2bcaf
shelest <shelest@example.com>
2019-08-10 14:27:20 +0300

    Initial commit

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -153,7 +153,7 @@ if os.getcwd() == '/app':
 
 	# Static asset configuration
 	BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-	STATIC_URL = '/static/'
-	STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+	#STATIC_URL = '/static/'
+	STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 	STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),
 	)
