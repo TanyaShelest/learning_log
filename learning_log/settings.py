a15cd605fd27661cbd4853023d545e11e6557f7e
shelest <shelest@example.com>
2019-08-10 14:17:29 +0300

    Initial commit

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -139,22 +139,21 @@ BOOTSTRAP3 = {
 #django_heroku.settings(locals())
 
 # Heroku settings
-if os.getcwd() == '/app':
-	import dj_database_url
-	DATABASES = {
-		'default': dj_database_url.config(default='postgres://localhost')
-	}
-	
-	# Honor the 'X-Forwarded-Proto' header for request.is_secure().
-	SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-	
-	# Allow all host headers.
-	ALLOWED_HOSTS = ['*']
-	
-	# Static asset configuration
-	BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-	STATIC_URL = '/static/'
-	STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-	STATICFILES_DIRS = (
-		os.path.join(BASE_DIR, 'static'),
+#if os.getcwd() == '/app':
+#	import dj_database_url
+DATABASES = {
+	'default': dj_database_url.config(default='postgres://localhost')
+}
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure().
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers.
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),
 	)
