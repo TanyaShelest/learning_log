af046c4b90548bc189f3b84c2eb1d3412bc7cf6e
shelest <shelest@example.com>
2019-08-10 18:35:36 +0300

    Set DEBUG=False for Heroku.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'af^f(mgkfht$ydsu09c*t#_z=f)vrpp=w=ps5$z*j&+ad$q$72'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -150,7 +150,7 @@ if os.getcwd() == '/app':
 	# Allow all host headers.
 	ALLOWED_HOSTS = ['learnandlog-project.herokuapp.com']
 	
-	#DEBUG = False
+	DEBUG = False
 
 	# Static asset configuration
 	BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
