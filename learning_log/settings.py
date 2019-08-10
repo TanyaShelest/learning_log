b105785d3c67ab3084c63e15158760555eedeef1
shelest <shelest@example.com>
2019-08-10 14:53:19 +0300

    Initial commit

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -152,8 +151,9 @@ if os.getcwd() == '/app':
 	ALLOWED_HOSTS = ['*']
 
 	# Static asset configuration
-	BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-	#STATIC_URL = '/static/'
+	BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+	STATIC_URL = '/static/'
 	STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-	STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),
+	STATICFILES_DIRS = (
+		os.path.join(BASE_DIR, 'static'),
 	)
